069a967197215b28c000635b37d29949ccc3eda8
Pooja Shetty <poojaniranjanshetty@gmail.com>
2026-06-09 18:59:50 +0530

    FIX: Added few files

---
+++ static/css/bootstrap.min.css
@@ -0,0 +1,48 @@
+.left-panel {
+    width: 25%;
+    float:left;
+}
+
+.right-panel {
+    /* background-color:#ffffff; */
+}
+
+.a-self-links {
+    text-decoration:none;
+    float:left;
+    color:black;
+    margin-right:2%;
+}
+
+.profile-img {
+    width: 100px; /* Increase width to zoom in */
+    height: 120px;
+    border-radius: 50%;
+    /* border: 2px #333 solid; */
+    object-fit: cover;
+    object-position: center;
+}
+
+#a-tag {
+    text-decoration:none;
+}
+
+div hr {
+    margin-right:2%;
+}
+
+table td {
+    border:1px solid black;
+}
+
+dl dt {
+
+}
+
+dl dd {
+    margin-left:10px;
+    margin-bottom:0px;
+}
+.job-title {
+    font-size: 100%;
+}
